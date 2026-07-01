c7b7f567e0ebf1bdf766641fcd24dcfb015fd4a2
rahul-gitt <postbox9635@gmail.com>
2026-07-02 02:28:21 +0530

    Create account Function

---
+++ pj01-Bank_Managemant_system/main.py
@@ -0,0 +1,69 @@
+# Bank Management System
+import json
+import random
+import string
+from pathlib import Path
+
+class Bank:
+    database = 'C:\\python-phase-3\\pj01-Bank_Managemant_system\\customer_data.json'  # main json file path
+    data = []      # Dummy data
+    try:
+        if Path(database).exists():
+            with open(database) as f:
+                data = json.loads(f.read())
+        else : 
+            print("No such file exist..")
+
+    except Exception as err:
+        print(f"An error occured as {err}")
+    
+    @classmethod
+    def __update(cls):
+        with open(Bank.database,'w') as f:
+            f.write(json.dumps(Bank.data))
+    
+    @classmethod
+    def __accountgenerate(cls):
+        alpha = random.choices(string.ascii_letters,k=4)
+        num = random.choices(string.digits,k =4)
+        spchar = random.choices("@#$%&*",k = 1)
+        id = alpha+num+spchar
+        random.shuffle(id)   #it return as object
+        return "".join(id)   # I return it as a string.
+
+    def createaccount(self):
+            info = {
+                "name" : input("Enter Your Name : "),
+                "age"  : int(input("Enter your age : ")),
+                "email" : input("Enter your Email : "),
+                "phone" : int(input("Enter your Phone number : ")),
+                "pin" : int(input("Enter your 4 digit pin : ")),
+                "account_no." : Bank.__accountgenerate(),
+                "balence" : 0
+            }
+            if info['age'] <12 or len(str(info['pin']))!= 4:
+                print("You can't create your account..")
+            else:
+                print("Account created successfully...")
+                for i in info:
+                    print(f"{i} : {info[i]}")
+                print("Please note your account number")
+        
+                Bank.data.append(info)
+                Bank.__update()
+
+
+
+user = Bank()
+
+print("Press 1 for Creating Account. ")
+print("Press 2 for Depositing Money. ")
+print("Press 3 for Withdrawing Money. ")
+print("Press 4 for Account Details. ")
+print("Press 5 for Updatng Details. ")
+print("Press 6 for Deleting your Account. ")
+
+check = int(input("Tell your responce : "))
+
+if check == 1:
+    user.createaccount()
